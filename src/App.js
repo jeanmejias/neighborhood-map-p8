e9875559426c68f00a4c6c660c810b3ac00d2776
jeanmejias <jean.eg.mejias@hotmail.com>
2018-08-26 22:14:31 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  initMap = () => {
+    const map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+  }
+
+  render() {
+    return (
+      <main>
+<div id="map"></div> 
+      </main>
+      
+    )
+  }
+}
+
+export default App;
