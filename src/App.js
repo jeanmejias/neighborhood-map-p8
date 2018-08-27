8ff8d5b7b66983394a9f1e7325938d07fae49225
jeanmejias <jean.eg.mejias@hotmail.com>
2018-08-27 21:27:18 +0100

    Get venues from FourSquare API in React App using Axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
+  state = {
+    venues: []
+  }
+
+  componentDidMount() {
+    this.getveneus()
+    this.renderMap()
+  }
+
+  renderMap = () => {
+    loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyAtUrZiNp3fTVPcCyaEr2cMRUeTc6JJ_Ec&callback=initMap')
+    window.initMap = this.initMap
+  }
+
+  getveneus = () => {
+    const endPoint = 'https://api.foursquare.com/v2/venues/explore?'
+    const parameters = {
+      client_id: 'TX2XNCPJB4CNCKBRSVGRMFTVGZOKT1HXNZ2NFB5ANKIVDG4M',
+      client_secret:'CNCU2ZYY4DLW2VXMN231ZRYV2USO0LVZOLWC0E5KG1LLMJ3W',
+      query:'food',
+      near:'loughborough',
+      v: '20180323'
+    }
+
+    axios.get(endPoint + new URLSearchParams(parameters))
+    .then(response => {
+     this.setState({
+       venues: response.data.response.groups[0].items
+     })
+    })
+    .catch(error =>{
+      console.log('ERROR!! ' + error)
+    })
+  }
 
   initMap = () => {
-    const map = new google.maps.Map(document.getElementById('map'), {
+    var map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: -34.397, lng: 150.644},
       zoom: 8
     });
@@ -14,11 +48,22 @@ class App extends Component {
   render() {
     return (
       <main>
-<div id="map"></div> 
+<div id='map'></div> 
       </main>
       
     )
   }
 }
+
+
+function loadScript(url) {
+  var index  = window.document.getElementsByTagName('script')[0]
+  var script = window.document.createElement('script')
+  script.src = url
+  script.async = true
+  script.defer = true 
+  index.parentNode.insertBefore(script, index)
+}
+
 
 export default App;
