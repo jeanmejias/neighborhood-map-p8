e085021dc1c2f56a0252479acea86f53705dd0a8
jeanmejias <jean.eg.mejias@hotmail.com>
2018-08-28 22:37:39 +0100

    updated nav

---
+++ src/components/Nav.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    // console.log(this);
+  }
+
+  render() {
+    let menuText = this.props.sidebarOpen ? "Close" : "Open";
+    return (
+      <div id="navbar">
+        <h3 id="head-text">Neighborhood Maps</h3>
+        <h3 id="menu-text" className="transition" title={ menuText + " Sidebar" }
+          onClick={() => { this.props.toggleSideBar(!this.props.sidebarOpen) }}>
+          { menuText }
+        </h3>
+      </div>
+    );
+  }
+}
+
+export default Nav;
